ee8f2c9544db77677d41bab22cc7e4f31f26ec7d
Yohanna Tesfay <yohannatesfay99@gmail.com>
2020-12-02 23:29:03 -0800

    update

--- platformer/Area2D.gd
+++ platformer/Area2D.gd
@@ -2,8 +2,6 @@ extends Area2D
 
 export var value = 1
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimatedSprite.play("coins")
 
 func _on_coin1_body_entered(body):
 	# was it the player?
